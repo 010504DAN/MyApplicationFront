52357007a780b38ab52680b48f79ebe4a812c0ad
azatDzholoev <azat.zholoev@gmail.com>
2024-11-28 17:59:08 +0600

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationfront/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.myapplicationfront;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myapplicationfront.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        binding.button.setOnClickListener(v -> {
+            binding.lottie.setVisibility(View.VISIBLE);
+            binding.lottie.pauseAnimation();
+        });
+    }
+}
---
+++ app/src/main/java/com/example/myapplicationfront/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.example.myapplicationfront;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myapplicationfront.databinding.ActivityMainBinding;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        new Handler().postDelayed((Runnable) () -> {
+            startActivity(new Intent(SplashScreen.this, MainActivity.class));
+            finish();
+        }, 5000);
+    }
+}
